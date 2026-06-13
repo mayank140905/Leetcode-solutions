8cea8559a0817196fd778b60c56c1c363b58b86e
mayank140905 <mayank140905@gmail.com>
2026-06-13 13:43:10 +0530

    Time: 7 ms (99.03%), Space: 24.9 MB (78.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // we will use hashmap for this problem since, we will be grping
+        unordered_map <string, vector<string> > map;
+        // key , string = sorted string
+        // value, us key wale saare anagrams 
+
+        // first we need to go through all strings and grp them
+        for(string s : strs){
+            string temp = s;
+            sort(temp.begin(),temp.end());
+            
+            map[temp].push_back(s);
+        }
+        vector <vector <string>> ans;
+
+        for( auto &it:map){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
