b90d3565d209e8d7e364a7a0c341371dbeeedd74
mayank140905 <mayank140905@gmail.com>
2026-07-01 17:48:59 +0530

    Time: 554 ms (31.91%), Space: 178.9 MB (44.24%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        queue<pair<int,int>> q;
+
+        // Multi-source BFS
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i,j});
+                }
+            }
+        }
+
+        vector<int> dx = {1,-1,0,0};
+        vector<int> dy = {0,0,1,-1};
+
+        while(!q.empty()) {
+            auto [x,y] = q.front();
+            q.pop();
+
+            for(int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<n &&
+                   dist[nx][ny] == INT_MAX) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.push({nx,ny});
+                }
+            }
+        }
+
+        priority_queue<vector<int>> pq;
+        vector<vector<int>> best(n, vector<int>(n,-1));
+
+        pq.push({dist[0][0],0,0});
+        best[0][0] = dist[0][0];
+
+        while(!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+
+            int safe = cur[0];
+            int x = cur[1];
+            int y = cur[2];
+
+            if(x == n-1 && y == n-1)
+                return safe;
+
+            for(int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<n) {
+
+                    int newSafe = min(safe, dist[nx][ny]);
+
+                    if(newSafe > best[nx][ny]) {
+                        best[nx][ny] = newSafe;
+                        pq.push({newSafe,nx,ny});
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+};
