051045f423b7bbeeb55bcaa2ba840dd2ee56d780
mayank140905 <mayank140905@gmail.com>
2026-07-25 15:43:12 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.3%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    unordered_map <int,int> dp;
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
+        int a1 = climbStairs(n-1);
+        int a2 = climbStairs(n-2);
+        int ans = a1+ a2;
+        dp[n] = ans;
+        return ans;
+    }
+};
