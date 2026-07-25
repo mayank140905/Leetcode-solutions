282665132ddee9e85e1bcd25e8f7e7bd99443ad0
mayank140905 <mayank140905@gmail.com>
2026-07-25 09:38:14 +0530

    Time: 3 ms (65.17%), Space: 8.4 MB (11.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    // unordered_map <int,int> dp;
+    unordered_map <int,int> dp;
     int fib(int n) {
         if(n==1 || n==0){
             return n;
         }
 
-        // if(dp.find(n) != dp.end()){
-        //     return dp[n];
-        // }
+        if(dp.find(n) != dp.end()){
+            return dp[n];
+        }
 
         int a1 = fib(n-1);
         int a2 = fib(n-2);
         int ans = a1+a2;
-        // dp[n] = ans;
+        dp[n] = ans;
         return ans; 
     }
 };
